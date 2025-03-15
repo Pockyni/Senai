0420ef57b763ff6b7402fa9156b0e50dc176018e
Nicole <159788853+Pockyni@users.noreply.github.com>
2025-03-15 14:17:21 -0300

    Add files via upload

---
+++ ex01.js
@@ -0,0 +1,20 @@
+let preco = 250
+let desconto = 15 / 100
+
+// 250 * 0.15
+let valorDesconto = preco * desconto
+let valorTotal = preco - valorDesconto
+
+
+console.log( 'o valor de desconto:', valorDesconto)
+console.log( 'o valor TOTAL:', valorTotal)
+
+//Operadores de comparação problema 2
+
+
+
+
+
+
+
+
